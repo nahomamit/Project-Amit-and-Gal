7a4997745b08bf846dffc48dda7b6900e9c90264
nahomamit <nahomamit@gmail.com>
2021-04-26 16:38:17 +0300

    2

---
+++ app/src/main/java/com/example/final_project_amit_and_gal/ChooseExc.kt
@@ -0,0 +1,11 @@
+package com.example.final_project_amit_and_gal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseExc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_exc)
+    }
+}
--- app/src/main/java/com/example/final_project_amit_and_gal/MainActivity.kt
+++ app/src/main/java/com/example/final_project_amit_and_gal/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.final_project_amit_and_gal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.short_time)
+        button.setOnClickListener {
+            val intent = Intent(this, ChooseExc::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
