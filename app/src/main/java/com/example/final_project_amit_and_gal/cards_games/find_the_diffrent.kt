ab711a0df45a8d0ca52f000a5fed03abc4b9c19b
nahom <nahomamit@gmail.com>
2021-05-26 09:39:31 +0300

    Amit: move strings to string.xml

--- app/src/main/java/com/example/final_project_amit_and_gal/cards_games/find_the_diffrent.kt
+++ app/src/main/java/com/example/final_project_amit_and_gal/cards_games/find_the_diffrent.kt
@@ -16,49 +16,43 @@ class find_the_diffrent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_the_diffrent)
+
         val time:String = intent.getStringExtra("time").toString()
         val questions:Int = time.toInt()
         val score:Int = intent.getStringExtra("score").toInt()
+
         val score_text = findViewById<TextView>(R.id.score)
         score_text.text = score.toString()
+
         val time_left = findViewById<TextView>(R.id.remaining_questions)
-        time_left.text = "שאלות שנותרו:" + time
-        val text = findViewById<TextView>(R.id.task_for_costumer)
-        text.text = "מי יוצא מן הכלל?"
+        time_left.text = getString(R.string.remain_questions) + time
+
+        val task = findViewById<TextView>(R.id.task_for_costumer)
+        task.text = getString(R.string.who_is_diff)
+
         val back = findViewById<ImageView>(R.id.return_btn)
-        back.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage("האם אתה מעוניין לסיים את התרגול ולחזור לתפריט הראשי?")
-                .setCancelable(false)
-                .setPositiveButton("כן") { dialogInterface: DialogInterface, i: Int ->
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                }
-                .setNegativeButton("לא") { dialog, id ->
-                    // Dismiss the dialog
-                    dialog.dismiss()
-                }
-            val alert = builder.create()
-            alert.show()
-        }
+        back.setOnClickListener { backBtnOnClick() }
 
         val correct_ans = findViewById<Button>(R.id.ans_1)
         correct_ans.setOnClickListener {
             correct_ans.setBackgroundResource(R.color.Green)
             nextActivity(1,questions, score)
         }
+
         val wrong_ans = findViewById<Button>(R.id.ans_2)
         wrong_ans.setOnClickListener{
             wrong_ans.setBackgroundResource(R.color.colorAccent)
             correct_ans.setBackgroundResource(R.color.Green)
             nextActivity(0,questions, score)
         }
+
         val wrong_ans2 = findViewById<Button>(R.id.ans_3)
         wrong_ans2.setOnClickListener{
             wrong_ans2.setBackgroundResource(R.color.colorAccent)
             correct_ans.setBackgroundResource(R.color.Green)
             nextActivity(0,questions, score)
         }
+
         val wrong_ans3 = findViewById<Button>(R.id.ans_4)
         wrong_ans3.setOnClickListener{
             wrong_ans3.setBackgroundResource(R.color.colorAccent)
@@ -82,5 +76,21 @@ class find_the_diffrent : AppCompatActivity() {
         intent.putExtra("time",(questions-1).toString())
         intent.putExtra("score",(score+num).toString())
         startActivity(intent)
+    }
+
+    fun backBtnOnClick() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(getString(R.string.sure_end_excercize))
+            .setCancelable(false)
+            .setPositiveButton(getString(R.string.yes)) { dialogInterface: DialogInterface, i: Int ->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            .setNegativeButton(getString(R.string.no)) { dialog, id ->
+                // Dismiss the dialog
+                dialog.dismiss()
+            }
+        val alert = builder.create()
+        alert.show()
     }
 }
