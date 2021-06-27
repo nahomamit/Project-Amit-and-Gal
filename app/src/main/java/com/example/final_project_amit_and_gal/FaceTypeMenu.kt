25eb3ff8b86b4402defa89f3186737446266e7a7
nahom <nahomamit@gmail.com>
2021-06-27 16:59:26 +0300

    FaceTypeMenu.kt adding back button functionality

--- app/src/main/java/com/example/final_project_amit_and_gal/FaceTypeMenu.kt
+++ app/src/main/java/com/example/final_project_amit_and_gal/FaceTypeMenu.kt
@@ -1,11 +1,13 @@
 package com.example.final_project_amit_and_gal
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.final_project_amit_and_gal.cards_games.*
@@ -16,7 +18,25 @@ class FaceTypeMenu : AppCompatActivity() {
         setContentView(R.layout.activity_face_type_menu)
         buttonText()
         buttonOptions()
+        back_btn()
     }
+    fun back_btn(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("האם אתה מעוניין לסיים את התרגול ולחזור לתפריט הראשי?")
+            .setCancelable(false)
+            .setPositiveButton("כן") { dialogInterface: DialogInterface, i: Int ->
+                val intent = Intent(this, MainMenu::class.java)
+                startActivity(intent)
+            }
+            .setNegativeButton("לא") { dialog, id ->
+                // Dismiss the dialog
+                dialog.dismiss()
+            }
+        val alert = builder.create()
+        alert.show()
+    }
+
     fun buttonText() {
         findViewById<Button>(R.id.type_eyes).text = getString(R.string.face_eyes)
         findViewById<Button>(R.id.type_jaw).text = getString(R.string.face_jaw)
